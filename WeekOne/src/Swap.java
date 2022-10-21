361869671635ea11202e700194e67b54ddfa6de3
Anish Gyawali <anish.gyawali7@gmail.com>
2022-10-20 21:02:03 -0500

    swapping the two number

---
+++ WeekOne/src/Swap.java
@@ -0,0 +1,20 @@
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=200;
+		int b=300;
+		System.out.println("Before swapping");
+		System.out.println("a= "+ " "+a+" "+ "b= "+b);
+		
+		int temp=0;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After swapping");
+		System.out.println("a= "+ " "+a+" "+ "b= "+b);
+		
+	}
+
+}
