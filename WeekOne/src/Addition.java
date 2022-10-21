2951a959dc7064c7a150e0ba2d479b41166d91a9
Anish Gyawali <anish.gyawali7@gmail.com>
2022-10-20 20:17:56 -0500

    basic addition

---
+++ WeekOne/src/Addition.java
@@ -0,0 +1,13 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100;
+		int b=200;
+		int sum =a+b;
+		System.out.println("Sume of twp number is "+sum);
+	}
+
+}
