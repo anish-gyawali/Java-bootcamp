559eeec64a996f91ea8d2703ef4cc72260be6692
Anish Gyawali <anish.gyawali7@gmail.com>
2022-10-20 20:27:10 -0500

    basic sub and multiplication

---
+++ WeekOne/src/Subtraction.java
@@ -0,0 +1,13 @@
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=100;
+		int b=50;
+		int subtract=a+b;
+		System.out.println("Subtraction "+subtract);
+	}
+
+}
