c3e2e5bb8bc7219f81b86fc7aa1605892299d947
Anish Gyawali <anish.gyawali7@gmail.com>
2022-10-20 20:06:49 -0500

    simple use of local variable and print

---
+++ WeekOne/src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//local variable
+		int sno=100;
+		float avg=25.25f;
+		String name ="Anish";
+		boolean isPass=true;
+		long phone_number=98430778l;
+		System.out.println(sno+"\t"+avg+"\t"+name+"\t"+isPass+"\t"+phone_number);
+	}
+
+}
