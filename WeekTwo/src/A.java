415f179cd58d229486aa560c0e58a0942ed5e6ab
Anish Gyawali <anish.gyawali7@gmail.com>
2022-10-23 21:05:53 -0500

    example of NullPointException

---
+++ WeekTwo/src/A.java
@@ -0,0 +1,10 @@
+
+public class A {
+	int i=100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A b= null; //NullPointException
+		System.out.println(b.i);		
+	}
+
+}
