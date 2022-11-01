95b6e17768a05401a4505d6248f4ffed466db76d
Anish Gyawali <anish.gyawali7@gmail.com>
2022-10-31 19:35:11 -0500

    use of left and right shift operaters

---
+++ WeekThree/src/com/shift/LeftShiftEx.java
@@ -0,0 +1,13 @@
+package com.shift;
+
+public class LeftShiftEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//  '<<'  is used to shift all the bits in a value to left side of specified number
+
+		System.out.println(10 << 2); // 10*2^2= 10*4=40
+		System.out.println(10 << 3); // 10*2^3= 10*8=80
+	}
+
+}
---
+++ WeekThree/src/com/shift/RightShiftEx.java
@@ -0,0 +1,13 @@
+package com.shift;
+
+public class RightShiftEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// '>>' is used to move the value of the left operand to right by the number of its specified by right operand
+
+		System.out.println(20 >> 2); // 20/2^2 =20/4(2*2)=5
+		System.out.println(10 >> 5); // 10/2^5=10/32 (2*2*2*2*2)=0
+	}
+
+}
