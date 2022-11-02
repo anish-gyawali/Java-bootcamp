8b160fbf0dc6bd14402a07225320c696ba3bbc7c
Anish Gyawali <anish.gyawali7@gmail.com>
2022-11-01 20:15:36 -0500

    use of for loop

---
+++ WeekThree/Loop/src/com/loop/ForLoop.java
@@ -0,0 +1,19 @@
+/*even and odd using for loop*/
+package com.loop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (i=0;i<10;i++){
+			if (i%2==0){
+			System.out.println("Even number: "+ i);
+			}
+			else{
+			System.out.println("Odd number: "+ i);
+			}
+		}
+	}
+
+}
