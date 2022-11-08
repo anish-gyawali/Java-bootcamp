322aca95b825f108971d47cbbcee21bb7d080088
Anish Gyawali <anish.gyawali7@gmail.com>
2022-11-07 19:27:33 -0600

    simple use of for each

---
+++ WeekFour/Array/src/com/array/ArrayExSix.java
@@ -0,0 +1,15 @@
+package com.array;
+
+public class ArrayExSix {
+
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {1,2,34,5,6,98};
+	
+		//For each
+		for(int i: arr) {
+			System.out.println(i);
+		}
+	}
+
+}
