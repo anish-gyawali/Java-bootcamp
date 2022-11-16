e42e0b4899f6ccb8265deeee2c30f8058c1270e5
Anish Gyawali <anish.gyawali7@gmail.com>
2022-11-15 18:31:35 -0600

    Use or Encapsulation with example

---
+++ WeekFive/OOPS/src/com/encap/Encapsulation.java
@@ -0,0 +1,17 @@
+
+//Encapsulation is the process of hiding data members 
+//It has private data member with getter and setter method
+package com.encap;
+
+public class Encapsulation {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ WeekFive/OOPS/src/com/encap/TestEncapsulate.java
@@ -0,0 +1,12 @@
+package com.encap;
+
+public class TestEncapsulate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation encap=new Encapsulation();
+		encap.setName("Anish");
+		System.out.println(encap.getName());
+	}
+
+}
