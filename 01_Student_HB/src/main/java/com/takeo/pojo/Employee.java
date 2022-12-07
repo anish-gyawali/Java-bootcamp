edbb95c151c2d4342f99d20a9ac212bab4dd06a5
Anish Gyawali <anish.gyawali7@gmail.com>
2022-12-07 16:02:21 -0600

    first hibernate app

---
+++ 01_Student_HB/src/main/java/com/takeo/pojo/Employee.java
@@ -0,0 +1,47 @@
+package com.takeo.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+
+@Table(name = "EMP_DETAILS")
+public class Employee {
+
+	@Id
+	@Column(name = "ENO")
+	private int eno;
+
+	@Column(name = "ENAME")
+	private String ename;
+
+	@Column(name = "EADDRESS")
+	private String eaddress;
+
+	public int getEno() {
+		return eno;
+	}
+
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getEaddress() {
+		return eaddress;
+	}
+
+	public void setEaddress(String eaddress) {
+		this.eaddress = eaddress;
+	}
+
+}
